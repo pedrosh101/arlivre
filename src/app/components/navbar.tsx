51e0b141cda8c1800dfbab5e103138e1abfeaf9f
pedrosh101 <pedro.sh101@gmail.com>
2023-12-06 17:20:42 -0300

    2ndcss

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
       >
         <div
           ref={navRef}
-          className={`fixed flex flex-col space-y-6 right-0 top-0 h-full w-72 sm:w-80 p-10 bg-white shadow transition-transform transform duration-700 text-black ${
+          className={`fixed flex flex-col space-y-6 right-0 top-0 h-full w-72 sm:w-80 p-10 bg-orange-100 shadow transition-transform transform duration-700 text-black ${
             isOpen ? "translate-x-0" : "translate-x-full"
           }`}
         >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center  sm:p-24 p-10 bg-orange-200 text-black text-5xl">
+      <main className="flex min-h-screen flex-col items-center  sm:p-24 p-10 bg-orange-200 text-black sm:text-5xl text-4xl">
         <h1 className="font-gotham text-center">Ar Livre Edições</h1>
-        <div className="p-8 mt-10 font-gotham sm:w-4/12 bg-amber-900 text-orange-200 text-2xl leading-relaxed hover:text-orange-300 transition ease-in-out duration-700">
+        <div className="p-8 mt-10 sm:w-4/12  text-black text-2xl leading-relaxed hover:text-red-300 italic transition ease-in-out duration-1000 border-2 border-red-300">
           <p>Têm como projeto coleções dedicadas a ensaios sobre </p>
-          <p className="pl-8">Arte</p>
+          <p className="pl-8 pt-4">Arte</p>
           <p className="text-end pr-20">Política</p>
           <p className="pl-14">Cultura do Presente</p>
-          <p className="text-end">E narrativas norteadas</p>
+          <p className="text-end pt-4">E narrativas norteadas</p>
           <p className="pl-14">por traços de </p>
           <p className="text-end">experimentalidade/criação de linguagens.</p>
 
