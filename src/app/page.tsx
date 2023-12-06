41e9a6c234610865f18e77d370ecd6fc368aa3db
pedrosh101 <pedro.sh101@gmail.com>
2023-12-06 17:24:26 -0300

    lighter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Navbar />
       <main className="flex min-h-screen flex-col items-center  sm:p-24 p-10 bg-orange-200 text-black sm:text-5xl text-4xl">
         <h1 className="font-gotham text-center">Ar Livre Edições</h1>
-        <div className="p-8 mt-10 sm:w-4/12  text-black text-2xl leading-relaxed hover:text-red-300 italic transition ease-in-out duration-1000 border-2 border-red-300">
+        <div className="p-8 mt-10 sm:w-4/12  text-black text-2xl leading-relaxed hover:text-red-300 italic transition ease-in-out duration-1000 border-2 border-red-300 font-light">
           <p>Têm como projeto coleções dedicadas a ensaios sobre </p>
           <p className="pl-8 pt-4">Arte</p>
           <p className="text-end pr-20">Política</p>
