263ed13123c3125bc666bd2b1f8794964faeb01e
pedrosh101 <pedro.sh101@gmail.com>
2025-11-26 16:13:32 -0300

    adquirirButton

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -1,8 +1,9 @@
 export const projects = [
-
   {
     id: 100,
     title: "Bebete Bezos",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20Bebete%20Bezos",
     author: "Mauricio Salles Vasconcelos",
     description:
       "Parte deste livro foi publicada, em 12 episódios (com andamento de folhetim), no site Lápis https://www.lapislaboratorio.site/, trazendo a designação de Post Tela-Romance. O projeto se centra numa recriação, no feminino, de Beto Rockfeller (telenovela emblemática em seu experimentalismo), acrescida de motivos romanescos extraídos de autores como Alfred Döblin e Witold Gombrowicz. Em pauta, se encontra uma atualização de personagens do século XX instalados numa época multimidiática gravitada em torno dos domínios de imagem/influência/informação tal como configura Jeff Bezos com seu diversificado, abrangente império empresarial (em correspondência neomilenar com o que Nelson Rockefeller exercia nos anos 1960 sobre o protagonista de Bráulio Pedroso).",
@@ -17,6 +18,8 @@ export const projects = [
   {
     id: 101,
     title: "Mutações da Escrita na Época do Vampirismo Pornográfico",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20Mutações%20da%20Escrita%20na%20Época%20do%20Vampirismo%20Pornográfico",
     author: "Tiago Cfer",
     description:
       "Escrito no ano de saída do confinamento pela covid-19 e retomada das atividades não essenciais ao modo presencial. Uma breve reflexão sobre o cinema do século XX, sob a ótica do vampirismo e da pornografia, lança questões para se pensar o isolamento global e a telemediação/digitalização da vida durante a pandemia de 2020. Em contraponto ao estado de asfixia que governanças sanitárias e financistas - com a ameaça da espécie no horizonte das telas - proporcionam à existência, o autor aborda livros de Samuel Beckett, Campos de Carvalho, Valêncio Xavier e Mauricio Salles Vasconcelos numa tentativa de diagramar, com a análise da criação romanesca destes autores, vias existenciais alternativas aos impasses  da época.",
@@ -31,6 +34,9 @@ export const projects = [
   {
     id: 102,
     title: "A Lágrima Macrológica (Hospital Brasil)",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20A%20Lágrima%20Macrológica%20(Hospital%20Brasil)",
+
     author: "Mauricio Salles Vasconcelos",
     description:
       "Um ensaio centrado nos ativismos minoritários durante o Não-governo (2019-2022). No epicentro da Pandemia do Corona Vírus, o País vivenciou um duplo lockdown, tanto político quanto sanitário. Toda uma condição crônica de parasitismo ideológico, intelectivo e cultural vem contagiando nossa nação, numa condição postergada de convalescença. A blindagem macrológica contra mutações sociais impulsiona um pranto infindo ante os poderes soberanos, que as posturas identitárias tornadas mainstream da vida presente  acirram. Um círculo tautológico se forma. Macrolágrima do Logos Technoglobalcapitalista se sobrepõe aos devires de coletivos heterogêneos.",
@@ -45,6 +51,9 @@ export const projects = [
   {
     id: 103,
     title: "Livro-Lápis",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20Livro-Lápis",
+
     author: "Mauricio Salles Vasconcelos - Carolina Zuppo Abed (Organizadores)",
     description:
       "Este livro reúne alguns textos de crítica (de arte e cultural) produzidos por integrantes dos Laboratórios de Criação Escrita de Literatura e Teoria - Linha de Pesquisa do Programa de Pós-graduação em Estudos Comparados de Literaturas de Língua Portuguesa (FFLCH/Universidade de São Paulo). Foram postados no site LÁPIS: https://www.lapislaboratorio.site/",
@@ -59,6 +68,9 @@ export const projects = [
   {
     id: 104,
     title: "Diwan",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20Diwan",
+
     author: "Mario Bellatin",
     description:
       "Termo de origem persa, Diwan remete a uma junção de textos diversos de um mesmo escriba (dibir) e reunidos em uma única obra. Nas duas narrativas presentes no livro, O palácio e Um kafkafarabeu, que podem ser lidas como duas histórias de amor, orbitam personagens, situações e temas recorrentes no universo do autor: o poeta velho e sua caderneta; o cachorro perezvón; a filósofa travesti; o salão de beleza tornado morredouro; a história da máquina Underwood 1915. Mas não se trata, ao menos não no caso de Bellatin, de uma antologia, e sim, de uma reescritura que agrega movimento a uma ritualística da escrita, entre repetição e inventividade - “Você nunca pode alcançar a santidade. Vai escrever e escrever.” (O palácio).",
@@ -73,6 +85,9 @@ export const projects = [
   {
     id: 105,
     title: "O Asaro",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20O%20Asaro",
+
     author: "Jacques Stern",
     description:
       "O Asaro, de Jacques Stern, autor pouco conhecido da geração beat, experimentador de novas dimensões de escrita, é o primeiro volume da série Vírus. Cultuado por William S. Burroughs, Stern comparece com uma espécie de livro invisível, pouquíssimo conhecido, no entanto seminal quando se atenta para sua rede de relações com a contemporaneidade. Centrais para a criação dos cut-ups concebidos por Burroughs – uma prática decisiva na escrita, na cultura da mixagem e da apropriação, característica do contexto digital de agora –, Stern e seu The Fluke (publicado uma única vez, em 1965, e fora do mercado) marcam um encontro com muitas linhas produtivas de criação da atualidade. Em seu diálogo com Céline, Beckett, a deriva existencial desde os anos 1960 e a abertura para vertentes inovadoras de arte e comportamento, O asaro tem o impacto de uma revelação tardia. Soa como uma total novidade, em sua primeira tradução.",
@@ -87,6 +102,9 @@ export const projects = [
   {
     id: 106,
     title: "O Nado",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20O%20Nado",
+
     author: "Amélia Loureiro",
     description:
       "O nado é um conjunto narrativo inovador que incursiona por mapeamentos do cotidiano neomilenar. Realizando um jogo atraente e arguto entre anotação e reflexão, a escritora captura o contexto altamente tecnificado de hoje. Ao mesmo tempo, se alia a sondagens impensáveis do universo feminino. Encontra-se em sintonia com os signos da mundialização e da ambiência multicultural de hoje. Como destaca a ensaísta Sabrina Sedlmayer, em sua apresentação de O nado: “Se há, aqui, muito da força do Modernismo literário e seu ímpeto de invenção, deve-se lembrar que nihilo nihil: nada surge do nada. Amélia Loureiro tem escuta e sabe que ser campeão de natação e não saber nadar não é aporia nem contradição. Nadar, sem se sentir nadador. Preocupar-se com o processo, como Beckett de Worstward Ho. Amélia Loureiro está vitalmente comprometida com o trabalho de escrita. Parece viver em estado de arte”.",
@@ -101,6 +119,9 @@ export const projects = [
   {
     id: 107,
     title: "Kafka de férias",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20Kafka%20de%20férias",
+
     author: "Damián Tabarovsky",
     description:
       "Nessa narrativa do escritor e ensaísta argentino Damián Tabarovsky, em sintonia com muitas teses de seu importante ensaio Literatura de esquerda, a tradição kafkiana se encontra em jogo, no auge do desencanto e da solidão contemporâneas. O texto vivaz, imantador, é capaz de rever a força da invenção referendada na modernidade, ao mesmo tempo em que captura um universo de valores e inquietações existenciais, de modo inaugural, vivamente firmado no presente.",
@@ -115,6 +136,9 @@ export const projects = [
   {
     id: 108,
     title: "Postar/Popstar",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20Postar/Popstar",
+
     author: "Teti Conrado",
     description:
       "Estreia na ficção de uma autora concentrada num painel de realidades urgentes. Toma grande voltagem no romance a entourage tecno dos dispositivos de escrita e relacionamento em compasso com as tensões do mundo globalizado. Intenso é o foco dado aos horizontes de existência, trabalho e vida interpessoal à volta de 3 ex-integrantes do movimento emo em SP. Embalado por referências pop, o livro incursiona pelos impasses da postagem infonáutica, esquadrinhando impasses, interferências as mais diferentes nos modos de ser e estar na hora de agora. À maneira de um scanner nervoso, eletrificado, contextualiza-se na cena de violência política do Brasil nas últimas décadas.",
@@ -129,6 +153,9 @@ export const projects = [
   {
     id: 109,
     title: "O menino proletário: 3 narrativas",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20O%20menino%20proletário:%203%20narrativas",
+
     author: "Osvaldo Lamborghini",
     description:
       "Pela primeira vez é publicado em português um livro com algumas das narrativas produzidas pelo argentino Osvaldo Lamborghini (1940-1985). Com um toque de Sade, outro mais de Céline, simultaneamente impregnado das fontes mais vivas e inquietantes da grande literatura latino-americana, o autor de O menino proletário propõe uma forma insurgente de escrita. O projeto de Lamborghini é capaz de fazer rever mapeamentos historiográficos do século XX até hoje através de cortes nos espaços/tempos envolvendo diferentes autorias. A contar de um perturbador engendramento construtivo/conceptivo, desponta com intenso poder de propagação o que pode ser a arte narrativa no corpo do presente, com a presentificação do corpo.",
@@ -143,6 +170,9 @@ export const projects = [
   {
     id: 110,
     title: "Catálise",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20Catálise",
+
     author: "Lucas Miyazaki",
     description:
       "Em seu romance de estreia, Miyazaki põe em cena sua proposição de escrita-performance, base de suas pesquisas no campo da Literatura Comparada. Faz irromper um texto centrado no teatro, inovando o sentido de narrativa, como também a teatralidade e o que se concebe como performativo. Um trabalho inaugural ganha uma mobilizante forma. Como dramaturgo, Lucas Miyazaki compôs e levou à cena as peças Não Ela: o que é bom está sempre sendo destruído (2019-2021), e os monólogos Incubação dos sonhos (2020), para uma experimentação video-cênica durante a pandemia, e Limpo (2021). Escreveu diversas narrativas, como “Radio Silence”, “História de como voltei ao seu lugar”, “Ar no parque”, entre outras, publicadas em revistas. É autor dos livros Elefantes (Prêmio Nascente 2015) e de CELULAR.QUITINETE.RUA (2017).",
@@ -157,6 +187,9 @@ export const projects = [
   {
     id: 111,
     title: "Gradiente Spectrum",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20Gradiente%20Spectrum",
+
     author: "Tiago Cfer",
     description:
       "Ao redesenhar as perspectivas da ficção-científica (legado do século XX), Cfer cria um diagrama dinâmico no presente milênio sobre as noções futuro e as regressões da ideia de progresso. Toda uma ideologia, tanto científica como fabular, política como existencial, está combinado no romance sob o signo da música progressiva (Kraut), centrando-se nos desvios de todos os tipos observados na realidade brasileira. Gradiente Spectrum parte de um esquema de antecipação (típico das ficções centradas no futuro) para especular a imensa rede de implicações tecnológicas no plano mais direto da realidade. Cria uma espécie de documento sobre o Brasil nos últimos anos, dentro de um presente acrônico/anacrônico anunciado por projeções futuras (nos domínios progressistas da ficção e da ciência).",
@@ -171,6 +204,9 @@ export const projects = [
   {
     id: 112,
     title: "BR-Clítoris",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20BR-Clítoris",
+
     author: "Ana Pavla",
     description:
       "A noção de feminização do mundo, estudada por Lacan e ampliada pelo psicanalista argentino Ernesto Sinatra, impulsiona os escritos de Ana Pavla. Em seu primeiro romance, a autora coloca uma mulher nas estradas nacionais em uma motocicleta enquanto examina os esplendores e a violência que sofre o espaço natural brasileiro. Uma mistura de documentário fílmico criada sob a influência das narrativas de Kathy Acker faz de BR-Clítoris um verdadeiro acontecimento na literatura recente em língua portuguesa, devido à sua audácia e refinada articulação conceitual, dirigida a múltiplas vias da arte, da política e do comportamento.",
@@ -185,6 +221,9 @@ export const projects = [
   {
     id: 113,
     title: "Mantra Muzak",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20Mantra%20Muzak",
+
     author: "Mauricio Salles Vasconcelos",
     description:
       "	O jogo criado entre mantra e muzak deixa ver o dado da repetição imanente à teatralidade, à medida que acentua a máquina de dispositivos e signos os mais diversos em suas renovadas récitas. Reengendra, com o clamor da urgência, as poéticas de teatrólogos como Isabel Câmara e José Vicente, como também o itinerário teatral da atriz Maria Della Costa. Concebe o desenho de vida-arte legado por Jean-Luc Godard, em um ato desenrolado entre a performance e o take fílmico. Algo que também se estende ao cinema de Yasujiro Ozu. Criam-se, em Mantra Muzak, texturas convulsivas dos embates acerca de política/ativismo depois do vírus fascimiliciano no Brasil (motriz da peça Convalescença). Contornos de coreografia e canto (tal como propõe Sônica) modulam andamentos de música eletrônica nos seus incontáveis ciclos e reciclagens. Em Andarilha, mostra-se mais vivamente a errância da busca de tempo e lugar, que permeia os demais textos numa variedade de focos, de ritmos, da musicalidade mesmo, através de peregrinações intermináveis, extremas caminhadas urbanas e joggings confrontadores. O teatro acelera um corpus verbal na mesma gradação em que incita a pausa reflexiva, a meditação – o intervalo corpóreo-gestual da cena –, a contar do mecanismo da repetição, inerente ao rito teatral. Ao compasso da vibração intercambiante do devir-teatro, do teatro em devir.",
@@ -199,6 +238,9 @@ export const projects = [
   {
     id: 114,
     title: "DURO",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20DURO",
+
     author: "Amélia Loureiro",
     description:
       "Em seu mais recente livro, Amélia Loureiro dá sequência ao projeto de inovação do que se entende como escrita de si. Desta vez, ao modular uma concepção muito própria do gênero romance, a autora imprime uma linguagem inventiva com forte marca de documentalidade e alta voltagem filosófica. Apresenta-se nesta obra a via muito singular de como a escritora se concentra nas relações com a natureza, a contemporaneidade tecnológica, os embates com uma história timbrada por brutalismo e violação, de modo a desbravar tanto uma poética quanto uma ecosofia da escrita.",
@@ -213,6 +255,9 @@ export const projects = [
   {
     id: 115,
     title: "GUERRA GLOBAL SUBÚRBIO SATÉLITE",
+    whats:
+      "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20GUERRA%20GLOBAL%20SUBÚRBIO%20SATÉLITE",
+
     author: "Mauricio Salles Vasconcelos / Marcus Siqueira",
     description:
       "Livro de poesia e álbum musical, o projeto apresenta uma livre leitura de Crack Wars - Literature, Addiction, Mania (1992), ensaio da filósofa Avital Ronell. Guerra Global Subúrbio Satélite dá continuidade ao diálogo poético e musical, empreendido em Livro Telefone Rua (2022) por Mauricio Salles Vasconcelos e Marcus Siqueira, com a obra da grande pensadora contemporânea (tendo como eixo The Telephone Book, 1989). O elo entre sonoridade/literatura/filosofia se reativa no presente projeto. O foco recai nas modalidades do global em várias esferas da linguagem, criação e vida (a contar da ênfase em conomia, guerra, tecnocultura), envolvendo as artes da música, da poesia e da performance na atualidade. Através do QR Code, constante na parte final deste volume, o acesso à composição criada a partir dos poemas, concebida em 13 faixas, será viabilizado.",
@@ -227,6 +272,7 @@ export const projects = [
   {
     id: 116,
     title: "Romance A Caminho - Narrativa em tempo presente",
+    whats: "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20ROMANCE%20A%20CAMINHO",
     author: "Mauricio Salles Vasconcelos / Felipe Souza [Organizadores]",
     description:
       "O Seminário transmitido pelo canal Youtube da FFLCH/USP (de 18 a 21 de julho de 2022), com o título “Romance A Caminho – Mutações de um gênero literário no presente”, propiciou a reunião de ensaios e trechos de narrativas contidos neste livro. Incontáveis vertentes de escrita conceituadas e elaboradas por pesquisadores de Literatura Comparada também romancistas, alguns com projetos iniciais, outros com obras já publicadas, trazem à cena de hoje um universo sempre posto em debate, quando não refutação, no entanto, presentificado com enorme produção em todo mundo. O Romance ganha aqui títulos vários e angulações provocadoras. A despeito do regime dominante audiovisual a impor segmentos de fabulação aos olhos fixados em serialidades, compactações cinemáticas, streamings  concebidos a partir de entrechos literários.",
@@ -236,11 +282,13 @@ export const projects = [
     pag: "325",
     price: "R$60,00",
     img: "/img/17.jpg",
-    buy: "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20ROMANCE%20A%20CAMINHO",
+    buy: "",
   },
   {
     id: 117,
     title: "África Potência - Exemplos, de João Vário",
+      whats: "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20África%20Potência",
+
     author: "Mauricio Salles Vasconcelos",
     description:
       "ÁFRICA POTÊNCIA -  Exemplos, de João Vário. Ensaio de Mauricio Salles Vasconcelos. Este é o primeiro livro escrito sobre a poética de João Vário, um dos heterônimos de João Manuel Varela, autor cabo-verdiano. Dá destaque ao dado de que Vário pauta seu projeto pela adoção de um encaminhamento propositivo na série de livros (9 publicados de um projeto de 12) intitulados Exemplos. Seu trabalho se destaca como um dos maiores escritos do período contemporâneo, compreendido este desde a segunda metade do século passado, com incidência crescente em nossa atualidade. Por meio de uma voltagem indagativa e escritural que faz da África um polo impactante de criação, poeticidade e exemplaridade são tão fortalecidas quanto intercambiadas para se recompor as noções de humano e seus documentos de vida-arte nos transcursos do tempo. Como expressa o ensaísta José Paulo Palumbo no Posfácio de África Potência, “Insurgente nos modos de tramar suas iluminações transdisciplinarizadas — Avital Ronell, Mbembe, Pál Pelbart, Agamben, Sloterdijk, entre outros pensadores —, trabalhando-as em ramificada rede, Mauricio Salles Vasconcelos estabelece uma leitura prismática daquela que se coloca como uma das grandes vozes da poesia escrita em língua portuguesa durante a derradeira metade do século XX, cuja obra, ainda não editada no Brasil e pouco discutida na academia, merece a sensível atenção demonstrada neste volume”",
@@ -250,8 +298,6 @@ export const projects = [
     pag: "171",
     price: "R$55,00",
     img: "/img/18.jpg",
-    buy: "https://wa.me/5511983833111?text=Tenho%20interesse%20no%20livro%20ROMANCE%20A%20CAMINHO",
+    buy: "",
   },
-
-
 ];
--- src/app/livro/[id]/page.tsx
+++ src/app/livro/[id]/page.tsx
@@ -15,7 +15,6 @@ const Livro = ({ params }: any) => {
 
   return (
     <>
-
       <main className="flex w-full min-h-screen text-white bg-clr3 justify-center">
         <div className="flex flex-col sm:w-3/5 sm:my-16 my-10 px-4">
           <div className="flex sm:flex-row flex-col h-5/6 sm:space-x-24">
@@ -33,25 +32,31 @@ const Livro = ({ params }: any) => {
             </div>
             {/* right */}
             <div className="flex flex-col sm:w-3/5 space-y-4 px-4 lg:px-0">
-      
-                          <h1
-              className="text-4xl"
-              dangerouslySetInnerHTML={{
-                __html: italicizeWords(livro?.title || "")
-              }}
-            />
+              <h1
+                className="text-4xl"
+                dangerouslySetInnerHTML={{
+                  __html: italicizeWords(livro?.title || ""),
+                }}
+              />
 
               <h1 className="text-base font-semibold">{livro?.author}</h1>
-            <h1
-              className="text-base pb-4"
-              dangerouslySetInnerHTML={{
-                __html: italicizeWords(livro?.description || "")
-              }}
-            />
+              <h1
+                className="text-base pb-4"
+                dangerouslySetInnerHTML={{
+                  __html: italicizeWords(livro?.description || ""),
+                }}
+              />
+              {livro?.buy && livro.buy.trim() !== "" && (
+                <a href={livro.buy} target="_blank">
+                  <button className="bg-clr1 hover:bg-pink-400 text-black py-2 px-4 rounded-sm w-fit shadow-md">
+                    Comprar
+                  </button>
+                </a>
+              )}
 
-              <a href={livro?.buy} target="_blank">
+              <a href={livro?.whats} target="_blank">
                 <button className="bg-clr1 hover:bg-pink-400 text-black py-2 px-4 rounded-sm w-fit shadow-md">
-                  Comprar
+                  Adquirir via WhatsApp
                 </button>
               </a>
             </div>
